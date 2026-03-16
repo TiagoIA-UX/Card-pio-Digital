2e242a4a62467df928250b16df21a10369a196ec
TiagoIA-UX <globemarket7@gmail.com>
2026-03-16 15:34:33 -0300

    fix: restaura export ImageUploader no index.ts (removido pelo formatter)

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,5 +1,12 @@
 // Shared components
-export { Skeleton, TemplateCardSkeleton, TemplateGridSkeleton, CheckoutSkeleton, OrderListSkeleton } from './loading-skeleton'
+export {
+  Skeleton,
+  TemplateCardSkeleton,
+  TemplateGridSkeleton,
+  CheckoutSkeleton,
+  OrderListSkeleton,
+} from './loading-skeleton'
 export { EmptyState } from './empty-state'
 export { StarRating, StarRatingCompact } from './star-rating'
+export { ImageUploader } from './image-uploader'
 export { ImageUploader } from './image-uploader'
